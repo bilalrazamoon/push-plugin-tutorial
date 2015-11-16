1b9247518a9c87332c59dae79019388858a2d442
Bilal Raza <hafizbilal112@gmail.com>
2015-11-16 22:36:53 +0500

    Injected ngCordova module

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.services','starter.controller'])
+angular.module('starter', ['ionic', 'ngCordova', 'starter.services','starter.controller'])
 
 .run(function($ionicPlatform, $rootScope, $cordovaPushV5) {
   $ionicPlatform.ready(function() {
